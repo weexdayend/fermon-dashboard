93511ec09436b1a0304ef7725fd8fed86e1060c7
root <root@srv511009.hstgr.cloud>
2024-04-19 17:33:02 +0000

    Handling preFlight

--- src/app/(main)/master/page.tsx
+++ src/app/(main)/master/page.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { DataTables } from '@/components/shared/table'
 import TabContents from '@/components/pages/master/tab-contents'
-import Event from './event'
 
 type Props = {}
 
@@ -10,7 +9,6 @@ function Master({}: Props) {
   return (
     <div className='w-full h-full flex flex-col gap-4'>
       <div className='grid grid-cols-2 w-full gap-4'>
-        <Event />
         <TabContents />
         <DataTables />
       </div>
